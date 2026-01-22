32c4c09ace3e33d724c28f32a210e9e58d8fc97d
Melody5Sun <sunwenjing513@gmail.com>
2026-01-22 16:36:26 +0800

    前端web框架

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Web Project Init</h1>
+      <p>Framework structure loaded successfully.</p>
+    </div>
+  );
+}
+export default App;
---
+++ web/src/utils/request.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosInstance, AxiosError, InternalAxiosRequestConfig } from 'axios';
+
+const service: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || '/api',
+  timeout: 10000,
+});
+
+service.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    // TODO: Add token logic here
+    return config;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  }
+);
+
+service.interceptors.response.use(
+  (response) => {
+    const res = response.data;
+    return res;
+  },
+  (error: AxiosError) => {
+    console.error('Request Error:', error);
+    return Promise.reject(error);
+  }
+);
+
+export default service;
